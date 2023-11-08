dcd3a37ad5c1f275211ff15ffa6d2b992ea1e2f2
expmo <53528719+EXPSTUDIOmo@users.noreply.github.com>
2023-11-08 11:48:47 +0100

    added 2. sound

--- server/public/js/client.js
+++ server/public/js/client.js
@@ -131,6 +131,11 @@ function loadSounds(voiceid)
     DBG(`loading sounds for voice ${voiceid}`);
 
     SOUNDS[0] = new Howl({
+        src: [`Samples/vincze/FL_${voiceid}.mp3`],
+        html5: true
+      }); 
+
+    SOUNDS[1] = new Howl({
         src: [`Samples/test/TS_${voiceid+1}.mp3`],
         html5: true
       }); 
